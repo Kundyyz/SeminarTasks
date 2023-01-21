55434a48a7e75e5e1b507ca75b9a84cae92b52bc
Kundyz <kundyz94@gmail.com>
2023-01-22 02:33:45 +0500

    Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход
+принимает число (N), а на выходе показывает все чётные
+числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+
+*/
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    Console.WriteLine("Введено ненатуральное число!");
+}
+else
+{
+    int count = 1;
+    if (count % 2 == 1)
+    count++;
+    while (count < number)
+        if (count % 2 == 0)
+        {
+            Console.Write($"{count}");
+            count++;
+        }
+
+        else
+        {
+            Console.Write(", ");
+            count++;
+        }
+    if (count == number)
+        if (count % 2 == 0)
+        {
+            Console.WriteLine($"{count}");
+        }
+        else
+        {
+            Console.WriteLine("");
+
+        }
+
+}

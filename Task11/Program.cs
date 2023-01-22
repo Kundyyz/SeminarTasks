6da76fa1a64ee40dc6351ae7d9495f1f72dd7e31
Kundyz <kundyz94@gmail.com>
2023-01-22 22:51:50 +0500

    Tasks

---
+++ Task11/Program.cs
@@ -0,0 +1,30 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+/*
+
+int number = new Random().Next(100,1000);
+Console.WriteLine($"Случайное трёхзначное число: {number}");
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+int result = firstDigit*10 + thirdDigit;
+Console.WriteLine($"{result}");
+*/
+
+int rendomNumber = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число: {rendomNumber}");
+int finishNumber = GetNumber(rendomNumber);
+Console.WriteLine($"Конечное число => {finishNumber}");
+
+int GetNumber(int number)
+{
+int firstNumber = number / 100;
+int secondNumber = number % 10;
+
+int result = firstNumber * 10 + secondNumber;
+return result;
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,20 @@
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если число 1 не кратно числу 2, то
+// программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите первое числа:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе числа:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int remainder = MultipleNum (firstNumber, secondNumber);
+if (remainder == 0) Console.WriteLine("Кратно");
+else Console.WriteLine($"Не кратно, остаток {remainder}");
+
+int MultipleNum (int num1, int num2)
+{
+    return num1 % num2;
+}

8e578c48f227ac08e5d076e0abfbc0253c414cf3
Kundyz <kundyz94@gmail.com>
2023-01-23 16:48:18 +0500

    Seminar 2

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int SecondDigit( int num)
+{
+    int result = num / 10 % 10;
+    return result; 
+}
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number / 100 > 0) && (number / 1000 == 0))
+{
+int secondDigit = SecondDigit(number);
+Console.WriteLine($"{secondDigit}");
+}
+else Console.WriteLine("Введено не трехзначное число!");

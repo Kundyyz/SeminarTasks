fa4bbf92bcd6aae8f3dea84604bd3f2082938695
Kundyz <kundyz94@gmail.com>
2023-01-26 02:56:07 +0500

    таси

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Напишите координаты двух точек");
+Console.Write("Первая точка X: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Первая точка Y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Первая точка Z: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Вторая точка X: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Вторая точка Y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Вторая точка Z: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+double distance = Distance(x1, y1, z1, x2, y2, z2);
+double distanceRound = Math.Round(distance, 2, MidpointRounding.ToZero);
+Console.WriteLine($"Расстояние между точками: {distanceRound}");
+
+double Distance(int xc1, int yc1, int zc1, int xc2, int yc2, int zc2)
+{
+    return Math.Sqrt(Math.Pow((xc1 - xc2), 2) + Math.Pow((yc1 - yc2), 2)+Math.Pow((zc1-zc2),2));
+}
+
--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<1) Console.WriteLine("Введено ненатуральное число");
+else Table (number);
+
+void Table(int num)
+{
+int count = 1;
+while (count <= num)
+{
+   Console.WriteLine($"{count,4}   {Math.Pow(count, 3),8}");
+count++;   
+}
+}

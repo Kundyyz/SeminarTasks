75f15895eab1a9eb205a2b7605f34fa017171c0b
Kundyz <kundyz94@gmail.com>
2023-01-27 01:50:10 +0500

    Task 24

--- Task24/Program.cs
+++ Task24/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = Sum (number);
+if (number > 0)
+Console.WriteLine($"Сумма чисел: {sum}");
+else Console.WriteLine("Некорректное число");
+
+int Sum (int num)
+{
+    int result = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        result = result + i;
+    }
+    return result;
+}

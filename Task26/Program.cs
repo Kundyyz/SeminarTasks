e7c24ff1ed0dd6be832061516fbfaf008d43518a
Kundyz <kundyz94@gmail.com>
2023-01-27 02:11:28 +0500

    Task 26

---
+++ Task26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5 
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int quantity = Quantity (number);
+Console.WriteLine($"{number} -> {quantity}");
+
+int Quantity(int num)
+{
+    int count = 0;
+    if (num<0) num = num * -1;
+    while (num > 0)
+    {
+        num = num/10;
+        count = count + 1;
+     }
+    return count;
+}
